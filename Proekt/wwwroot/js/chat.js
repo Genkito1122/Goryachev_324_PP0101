c058471a391be9a76827e684d87c40d8c6f4ccb1
Genkito1122 <vslera.2006@gmail.com>
2025-07-02 23:08:00 +0400

    Add files via upload

---
+++ Proekt/wwwroot/js/chat.js
@@ -0,0 +1,19 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("ReceiveMessage", (message) => {
+});
+
+connection.on("ReceiveAnnouncement", (announcement) => {
+});
+
+async function startConnection(classId) {
+    try {
+        await connection.start();
+        await connection.invoke("JoinClassGroup", classId);
+    } catch (err) {
+        console.error(err);
+    }
+}
